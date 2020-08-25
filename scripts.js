16cf288915bcdb4508db97df0019cbdc24f30b6d
Raynelle Francisca <raynelle.alphonso@theappbusiness.com>
2020-08-25 12:35:07 +0100

    Game logic added

--- scripts.js
+++ scripts.js
@@ -1,17 +1,55 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+
 function flipCard() {
-    let hasFlippedCard = false;
-    let firstCard, secondCard;
+      if (lockBoard) return;
+      if (this === firstCard) return;
+      this.classList.add('flip');
 
-    this.classList.add('flip');
-  
 
-   if (!hasFlippedCard) {
-         hasFlippedCard = true;
-         firstCard = this;
+      if (!hasFlippedCard) {
+            hasFlippedCard = true;
+            firstCard = this;
+            return;
       }
 
+      secondCard = this;
+      hasFlippedCard = false;
+
+      checkForMatch();
+
 }
+
+function checkForMatch() {
+      let isMatch = firstCard.dataset.food === secondCard.dataset.food;
+      isMatch ? disableCards() : unflipCards();
+}
+
+
+function disableCards() {
+      firstCard.removeEventListener('click', flipCard);
+      secondCard.removeEventListener('click', flipCard);
+}
+
+
+function unflipCards() {
+      lockBoard = true;
+      setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            lockBoard = false;
+      }, 1500);
+}
+
+(function shuffle() {
+      cards.forEach(card => {
+            let ramdomPos = Math.floor(Math.random() * 12);
+            card.style.order = ramdomPos;
+      });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
