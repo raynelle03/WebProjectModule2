defc723409c57a05c26b541d09084155e6df34a5
Raynelle Francisca <raynelle.alphonso@theappbusiness.com>
2020-08-28 11:57:36 +0100

    Added images, css and script files

---
+++ scripts/scripts.js
@@ -0,0 +1,55 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+
+
+function flipCard() {
+      if (lockBoard) return;
+      if (this === firstCard) return;
+      this.classList.add('flip');
+
+
+      if (!hasFlippedCard) {
+            hasFlippedCard = true;
+            firstCard = this;
+            return;
+      }
+
+      secondCard = this;
+      hasFlippedCard = false;
+
+      checkForMatch();
+
+}
+
+function checkForMatch() {
+      let isMatch = firstCard.dataset.food == secondCard.dataset.food;
+      isMatch ? disableCards() : unflipCards();
+}
+
+
+function disableCards() {
+      firstCard.removeEventListener('click', flipCard);
+      secondCard.removeEventListener('click', flipCard);
+}
+
+
+function unflipCards() {
+      lockBoard = true;
+      setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            lockBoard = false;
+      }, 1500);
+}
+
+(function shuffle() {
+      cards.forEach(card => {
+            let ramdomPos = Math.floor(Math.random() * 12);
+            card.style.order = ramdomPos;
+      });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
